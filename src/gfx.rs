8a74616d5342317160c50fa46dbe7b6ecb151dfe
hatsunearu <me@hatsuenaru.xyz>
2019-04-13 23:59:44 +0900

    refactored rendering fns, added texturing, and added buggy transparency

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,13 +1,16 @@
 
+extern crate num;
 
-use std::cmp;
+use std::f32;
 use std::mem;
 use core::ops::{Deref, DerefMut};
 use image::{ImageBuffer, Pixel};
+use image::{RgbImage, RgbaImage, Rgb, Rgba};
 
 use cgmath::prelude::*;
-use cgmath::{Vector3, Point3};
-    
+use cgmath::{Vector3, Point3, Point2};
+
+use crate::obj::Face;
 
 
 fn safe_put_pixel<P, C>(buf: &mut ImageBuffer<P, C>, x: i32, y: i32, color: P)
@@ -71,92 +74,88 @@ where
     }
 }
 
-pub fn draw_tri<P, C>(buf: &mut ImageBuffer<P, C>, points: [[i32; 2]; 3], color: P)
-where
-    P: Pixel + 'static,
-    P::Subpixel: 'static,
-    C: Deref<Target = [P::Subpixel]> + DerefMut
+pub fn draw_tri(buf: &mut RgbImage, zbuf: &mut Vec<f32>, face: &Face, tex_img: &RgbaImage, color: Rgba<u8>)
 {
-    let mut bboxmin: [i32; 2] = [(buf.width() - 1) as i32, (buf.height() - 1) as i32];
-    let mut bboxmax: [i32; 2] = [0, 0];
-    let clamp: [i32; 2] = [(buf.width() - 1) as i32, (buf.height() - 1) as i32];
+    let mut bboxmin: [f32; 2] = [f32::INFINITY, f32::INFINITY];
+    let mut bboxmax: [f32; 2] = [f32::NEG_INFINITY, f32::NEG_INFINITY];
+    let clamp: [f32; 2] = [(buf.width() - 1) as f32, (buf.height() - 1) as f32];
     
-    for p in &points {
-        for (c, min, max, clmp) in izip!(p, &mut bboxmin, &mut bboxmax, &clamp) {
-            *min = cmp::max(0, cmp::min(*min, *c));
-            *max = cmp::min(*clmp, cmp::max(*max, *c));
+    //println!("points: {:?}", points);
+    
+    for p in &face.verts {
+        // add perspective projection here?
+        let x = p.x;
+        let y = p.y; 
+        
+        for (c, min, max, clmp) in izip!([x, y].iter(), &mut bboxmin, &mut bboxmax, &clamp) {
+            *min = 0f32.max(min.min(*c));
+            *max = clmp.min(max.max(*c));
         }
     }
     
-    for x in bboxmin[0]..bboxmax[0]+1 {
-        for y in bboxmin[1]..bboxmax[1]+1 {
-            let bary_v = barycentric([x, y], points);
+    let bboxmin: [u32; 2] = [bboxmin[0] as u32, bboxmin[1].ceil() as u32];
+    let bboxmax: [u32; 2] = [bboxmax[0] as u32, bboxmax[1].ceil() as u32];
+    
+    //println!("bboxmin: {:?}, bboxmax: {:?}", bboxmin, bboxmax);
+    
+    
+    /*
+    let bboxmin: [i32; 2] = [0, 0];
+    let bboxmax: [i32; 2] = [buf.width() as i32, buf.height() as i32];
+    */
+    
+    for x in bboxmin[0]..=bboxmax[0] {
+        for y in bboxmin[1]..=bboxmax[1] {
+            let bary_v = barycentric([x as f32, y as f32], &face.verts);
+            
             if bary_v.x < 0. || bary_v.y < 0. || bary_v.z < 0. {
                 continue;
             }
-            buf.put_pixel(x as u32, y as u32, color);
+            
+            let z = &face.verts.iter().map(|p| p.z).sum::<f32>() + bary_v.sum();
+            //buf.put_pixel(x as u32, y as u32, color);
+
+            if zbuf[(x + y*buf.width()) as usize ] < z {
+                zbuf[(x + y*buf.width()) as usize] = z;
+                
+                let tex_color = if let &Some(tex_uvs) = &face.tex_uvs {
+                    interpolate_texel(&bary_v, &tex_uvs, tex_img)
+                }
+                else {
+                    Rgba{ data: [255, 255, 255, 0] }
+                };
+                
+                let old_pixel = buf.get_pixel(x as u32, y as u32);
+                let trans = 1. - (tex_color[3] as f32) / 255.;
+                let new_pixel = Rgb{ data: [
+                    num::clamp(tex_color[0] as f32 + old_pixel[0] as f32 * trans, 0., 255.) as u8, 
+                    num::clamp(tex_color[1] as f32 + old_pixel[1] as f32 * trans, 0., 255.) as u8, 
+                    num::clamp(tex_color[2] as f32 + old_pixel[2] as f32 * trans, 0., 255.) as u8
+                    ]};
+
+                buf.put_pixel(x as u32, y as u32, new_pixel);
+            }
+
         }
     }
     
     
 }
-/*
-pub fn add_v(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
-    let mut output = [0.; 3];
-    for (component_sum, output_n) in a.iter().zip(b).map(|(a_n, b_n)| a_n + b_n).zip(output.iter_mut()) {
-        *output_n = component_sum
-    }
-    output
-}
-
-pub fn sub_v(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
-    let mut output = [0.; 3];
-    for (component_sum, output_n) in a.iter().zip(b).map(|(a_n, b_n)| a_n - b_n).zip(output.iter_mut()) {
-        *output_n = component_sum
-    }
-    output
-}
-
-
-pub fn dotp(a: &[f32; 3], b: &[f32; 3]) -> f32 {
-    a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
-}
-
-pub fn crossp(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
-    [
-    a[1]*b[2] - a[2]*b[1],
-    a[2]*b[0] - a[0]*b[2],
-    a[0]*b[1] - a[1]*b[0]
-    ]
-}
-
-
-pub fn mag(a: &[f32; 3]) -> f32 {
-    a.iter().map(|x| x*x).sum::<f32>().sqrt()
-}
-
-pub fn normalize(a: &mut [f32; 3]) {
-    let a_mag = mag(a);
-    for a_n in a.iter_mut() {
-        *a_n /= a_mag;
-    }
-}
-*/
 
 // barycentric basis of the input point `p` on the triangle defined by `points`
-fn barycentric(p: [i32; 2], points: [[i32; 2]; 3]) -> Vector3<f32> {
+fn barycentric(p: [f32; 2], points: &[Point3<f32>; 3]) -> Vector3<f32> {
 
 
     let a = Vector3::new(
-    (points[2][0] - points[0][0]) as f32,
-    (points[1][0] - points[0][0]) as f32,
-    (points[0][0] - p[0]) as f32
+    (points[2].x - points[0].x) as f32,
+    (points[1].x - points[0].x) as f32,
+    (points[0].x - p[0]) as f32
     );
     
     let b = Vector3::new(
-    (points[2][1] - points[0][1]) as f32,
-    (points[1][1] - points[0][1]) as f32,
-    (points[0][1] - p[1]) as f32
+    (points[2].y - points[0].y) as f32,
+    (points[1].y - points[0].y) as f32,
+    (points[0].y - p[1]) as f32
     );
     
     let u = a.cross(b);
@@ -167,4 +166,18 @@ fn barycentric(p: [i32; 2], points: [[i32; 2]; 3]) -> Vector3<f32> {
     }
     
     Vector3::new(1. - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z)
+}
+
+fn interpolate_texel(coord_bary: &Vector3<f32>, uvs: &[Point2<f32>; 3], tex: &RgbaImage) -> Rgba<u8>
+{
+    let u = uvs[0].x * coord_bary.x + uvs[1].x * coord_bary.y + uvs[2].x * coord_bary.z;
+    let v = uvs[0].y * coord_bary.x + uvs[1].y * coord_bary.y + uvs[2].y * coord_bary.z;
+    
+    let u_max = tex.width() as f32;
+    let v_max = tex.height() as f32;
+    
+    let x_texel = (u_max * u).round() as u32;
+    let y_texel = (v_max * (1.-v)).round() as u32;
+    
+    tex.get_pixel(x_texel, y_texel).clone()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,54 @@ extern crate rand;
 #[macro_use]
 extern crate itertools;
 
-use std::cmp;
+use std::f32;
 use std::env;
+use std::cmp;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use image::{ImageBuffer, Rgb};
-use rand::prelude::*;
+//use rand::prelude::*;
 use cgmath::prelude::*;
 
-use cgmath::{Vector3, Point3};
+use cgmath::{Vector3, Point3, Point2};
 
 
 mod gfx;
+mod obj;
 use crate::gfx::*;
+use crate::obj::*;
 
 fn main(){
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {:?} [obj_name]", args[0]);
+    if args.len() < 3 {
+        println!("Usage: {:?} [obj_name] [tex_name]", args[0]);
         return;
     }
     
     
-    let file = File::open(&args[1]).unwrap();
-    let mut buf = BufReader::new(file);
+    let obj_file = File::open(&args[1]).unwrap();
+    let mut obj_buf = BufReader::new(obj_file);
     
-    let (verts, faces) = parse_obj(&mut buf);
+    let gfxobj = obj::parse_obj(&mut obj_buf);
     //println!("{:?}", verts);
+    
+    let tex_file = File::open(&args[2]).unwrap();
+    let mut tex_buf = BufReader::new(tex_file);
+    
+    let tex_img = image::load(tex_buf, image::ImageFormat::PNG).unwrap().to_rgba();
 
-    let imgx = 500;
-    let imgy = 500;
+    let imgx = 800;
+    let imgy = 600;
 
     let mut imgbuf: ImageBuffer<Rgb<u8>, _> = image::ImageBuffer::new(imgx, imgy);
+    let mut zbuf: Vec<f32> = vec![f32::NEG_INFINITY; imgx as usize * imgy as usize];
+    /*
     let white = image::Rgb{ data: [255, 255, 255] };
     let red = image::Rgb{ data: [255, 0, 0] };
     let green = image::Rgb{ data: [0, 255, 0] };
     let blue = image::Rgb{ data: [0, 0, 255] };
-
+    */
     /*
     draw_line(&mut imgbuf, 250, 250, 300, 400, red);
     draw_line(&mut imgbuf, 250, 250, 200, 400, blue);
@@ -60,37 +70,29 @@ fn main(){
     let mut xbias: f32 = 0.;
     let mut ybias: f32 = 0.;
     
-    if args.len() >= 4 {
-        xbias = args[2].parse().unwrap();
-        ybias = args[3].parse().unwrap();
+    if args.len() >= 5 {
+        xbias = args[3].parse().unwrap();
+        ybias = args[4].parse().unwrap();
     }
     
-    for face in faces {
+    for face in &gfxobj.faces {
     
-        let mut ind0 = face[0];
-        let mut ind1 = face[1];
-        let mut ind2 = face[2];
-
-        let v0 = verts[ind0];
-        let v1 = verts[ind1];
-        let v2 = verts[ind2];
-        
+        let mut face = obj::deindex_face(&face, &gfxobj);
         //let mut normal_v = crossp(&sub_v(&v2, &v0), &sub_v(&v1, &v0));
         //normalize(&mut normal_v);
         //println!("{:?} {:?} {:?}", v0, v1, v2);
-        let mut normal_v = (v2 - v0).cross(v1 - v0).normalize();
+        let normal_v = (face.verts[2] - face.verts[0]).cross(face.verts[1] - face.verts[0]).normalize();
         
         let intensity: f32 = normal_v.dot(light_dir);
         
-        let v0 = convert_coords(v0, xbias, ybias, imgx, imgy);
-        let v1 = convert_coords(v1, xbias, ybias, imgx, imgy);
-        let v2 = convert_coords(v2, xbias, ybias, imgx, imgy);
-    
+        face.verts[0] = convert_coords(face.verts[0], xbias, ybias, imgx, imgy);
+        face.verts[1] = convert_coords(face.verts[1], xbias, ybias, imgx, imgy);
+        face.verts[2] = convert_coords(face.verts[2], xbias, ybias, imgx, imgy);
         
         if intensity > 0. { 
     
-            draw_tri(&mut imgbuf, [v0, v1, v2], 
-            image::Rgb{ data: [cmp::min(255, (intensity * 255.0) as u8); 3] });
+            draw_tri(&mut imgbuf, &mut zbuf, &face, &tex_img,
+            image::Rgba{ data: [cmp::min(255, (intensity * 255.0) as u8); 4] });
     
         }
     /*
@@ -112,70 +114,29 @@ fn main(){
     
     
     image::imageops::flip_vertical(&imgbuf).save("render.png").unwrap();
+    
 }
 
-fn convert_coords(vertex: Point3<f32>, xbias: f32, ybias: f32, imgx: u32, imgy: u32) -> [i32; 2] {
-    let x0 = ((vertex.x+xbias) * imgx as f32/2. ) as i32; 
-    let y0 = ((vertex.y+ybias) * imgy as f32/2. ) as i32; 
-    [x0, y0]
+fn convert_coords(vertex: Point3<f32>, xbias: f32, ybias: f32, imgx: u32, imgy: u32) -> Point3<f32> {
+    let x0 = (vertex.x+xbias) * imgx as f32/2.; 
+    let y0 = (vertex.y+ybias) * imgy as f32/2.; 
+    Point3::new(x0, y0, vertex.z)
 }
 
 
-fn parse_obj<B>(buf: B) -> (Vec<Point3<f32>>, Vec<[usize; 3]>) 
-where
-    B: BufRead
-{
-    
-    let mut verticies: Vec<Point3<f32>> = Vec::new();
-    let mut face_vert_inds: Vec<_> = Vec::new();
-    
-    for (i, line) in buf.lines().enumerate() {
-        let line = line.unwrap();
-    
-        let split: Vec<_> = line.trim().split_whitespace().collect();
-        if split.len() == 0 {
-            continue;
-        }
-        
-        if split[0] == "v" {
-            if split.len() != 4 {
-                panic!("Warning: could not parse vertex at line number {:?}", i);
-            }
-            
-            if let (Ok(vx), Ok(vy), Ok(vz)) = (split[1].parse::<f32>(), split[2].parse::<f32>(), split[3].parse::<f32>()) {
-                let v = Point3::new(vx, vy, vz);
-                verticies.push(v);
-            }
-            else {
-                panic!("Warning: could not parse vertex at line number {:?}", i);
-            }
-        }
-        
-        else if split[0] == "f" {
-            if split.len() != 4 {
-                panic!("Warning: could not parse face at line number {:?}", i);
-            }
-            
-            let v0: Vec<_> = split[1].split("/").collect();
-            let v1: Vec<_> = split[2].split("/").collect();
-            let v2: Vec<_> = split[3].split("/").collect();
-            
-            let v0_v: i32 = v0[0].parse().unwrap();
-            let v1_v: i32 = v1[0].parse().unwrap();
-            let v2_v: i32 = v2[0].parse().unwrap();
 
-            let inds: [i32; 3] = [v0_v - 1, v1_v - 1, v2_v - 1];
-            
-            face_vert_inds.push(inds);
-        }
-    }
-    
-    let wrap_index = |i: &i32| if *i < 0 { (*i + verticies.len() as i32) as usize } else { *i as usize };
-    
-    let face_vert_inds: Vec<[usize; 3]> = 
-    face_vert_inds.iter()
-    .map(|[i0, i1, i2]| [wrap_index(i0), wrap_index(i1), wrap_index(i2)] )
-    .collect();
-
-    (verticies, face_vert_inds)
+/*
+struct Face<T> {
+    coords: [T; 3],
+    tex_uv: Option<[T; 3]>,
+    normals: Option<[T; 3]>
 }
+
+
+struct GfxObject {
+    faces: Vec<Face<usize>>,
+    verts: Vec<Point3<f32>>,
+    tex_uvs: Vec<Point2<f32>>,
+    normals: Vec<Vector3<f32>>
+}
+*/
